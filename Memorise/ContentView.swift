269090958be0611e6939534e1719ba28fc3a7588
Mark Blinder <markblndr@gmail.com>
2020-05-25 15:33:17 -0700

    Initial Commit

---
+++ Memorise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorise
+//
+//  Created by Mark Blinder on 5/25/20.
+//  Copyright © 2020 Mark Blinder. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
